32054160ec525a927c1f2c9510b4b4847b489ffe
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-06-16 14:54:22 +0000

    getters/setters formapagamento

--- T2_POO/ContratoDeJogos/src/dados/FormaPagamento.java
+++ T2_POO/ContratoDeJogos/src/dados/FormaPagamento.java
@@ -5,4 +5,22 @@ import java.util.*;
 public class FormaPagamento {
     private String numero;
     private Date validade;
+    
+    public FormaPagamento(String numero, Date validade) {
+        this.numero = numero;
+        this.validade = validade;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public Date getValidade() {
+        return validade;
+    }
+    public void setValidade(Date validade) {
+        this.validade = validade;
+    }
 }
