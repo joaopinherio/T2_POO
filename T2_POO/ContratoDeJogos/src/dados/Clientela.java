249f83a0dd1cd773b657e5b7a9dfe6822581ef48
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-06-18 00:46:17 +0000

    remocao sout's do inicializaClientes/correcao do i+=count no final do loop

--- T2_POO/ContratoDeJogos/src/dados/Clientela.java
+++ T2_POO/ContratoDeJogos/src/dados/Clientela.java
@@ -22,25 +22,6 @@ public class Clientela {
     }
 
     // CLIENTESINICIAL.CSV
-    public void inicializaIndividual() {
-        Individual ind1 = null;
-        Path arq1 = Paths.get("CLIENTESINICIAL.CSV");
-        BufferedReader reader = null;
-
-        try (ObjectInputStream iarq = new ObjectInputStream(Files.newInputStream(arq1))) {
-            ind1 = (Individual) iarq.readObject();
-            addCliente(ind1);
-            iarq.close();
-            // arq1.close();
-        } catch (ClassNotFoundException e) {
-            System.out.println("1Problema na leitura do arquivo" + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("2Problema na leitura do arquivo" + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("3Problema na leitura do arquivo" + e.getMessage());
-        }
-    }
-
     public void inicializaClientes(Path arq) {
         Corporativo corp1 = null;
         BufferedReader reader = null;
@@ -64,65 +45,46 @@ public class Clientela {
              * }
              * System.out.println("INDICE 6");
             */
-           System.out.println("TESTE IND PRE");
-            System.out.println(data[11]);
+
 
             // LOOP LEITURA/CADASTROS
             //int count = 0;
             //data.length - 5 -> comprimento total menos 5 (numero de itens no cabecalho do .csv)
             //logo se nao tem mais que 5 numeros de indice sobrando quer dizer que aquela iteracao eh a ultima
             for (int i = 0; i < data.length - 5; i++) {
-                //System.out.println("HA");
                 if (i > 5) { // 6 eh o indice em que comecam os valores
                     int count = 0;
                     int tipo = Integer.parseInt(data[i + 3]);
-                    System.out.println("tipo:" + tipo);
                     if(tipo == 1){
                         int numero = Integer.parseInt(data[i]); count++;
-                        System.out.println(numero);
                         String nome = data[i+count];count++; 
-                        System.out.println(nome);
                         String email = data[i+count];count+=2;
-                        System.out.println(email);
-                        String cpf = data[i+count];count++;
-                        System.out.println("cpf");
-                        System.out.println(cpf);
+                        String cpf = data[i+count];
 
                         Individual ind = new Individual(numero, nome, email, cpf);
-                        System.out.println("teste ind");
-                        System.out.println(ind.descrever());
                         cadastro.add(ind);
                     }
                     if(tipo == 2){
-                        System.out.println("2corp");
-                        System.out.println(i);
                         int numero = Integer.parseInt(data[i]); count++;
                         String nome = data[i+count];count++; 
-                        System.out.println(numero);
                         String email = data[i+count];count++;
-                        System.out.println(nome);
                         String cnpj = data[i+count];count++;
-                        String nomeFantasia = data[i+count];count++;
+                        String nomeFantasia = data[i+count];
                         
                         Corporativo corp = new Corporativo(numero, nome, email, cnpj, nomeFantasia);
-                        System.out.println("teste corp");
-                        corp.descrever();
                         cadastro.add(corp);
                     }
-                    System.out.println("LOOP OUT OF BOUND");
-                    System.out.println(count + "count");
-                    System.out.println(i);
-                    i += count -1;
-                    System.out.println("indice");
-                    System.out.println(i);
+
+                    //soma o numero de indices percorridos nessa iteracao do loop para que o indice esteja correto na proxima
+                    i += count;
                 }
             }
 
             reader.close();
         } catch (IOException e) {
-            System.out.println("2Problema na leitura do arquivo" + e.getMessage());
+            System.out.println("1Problema na leitura do arquivo" + e.getMessage());
         } catch (Exception e) {
-            System.out.println("3Problema na leitura do arquivo" + e.getMessage());
+            System.out.println("2Problema na leitura do arquivo" + e.getMessage());
         }
     }
 
