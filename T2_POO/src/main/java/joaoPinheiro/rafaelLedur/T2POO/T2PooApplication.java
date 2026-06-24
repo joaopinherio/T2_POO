e286ff7412fb65b0a666cde1cc593eb9f20c1879
joaopinheiro <joaopdepaula@gmail.com>
2026-06-24 00:54:46 -0300

    spring boot setup

---
+++ T2_POO/src/main/java/joaoPinheiro/rafaelLedur/T2POO/T2PooApplication.java
@@ -0,0 +1,13 @@
+package joaoPinheiro.rafaelLedur.T2POO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2PooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2PooApplication.class, args);
+	}
+
+}
---
+++ T2_POO/src/test/java/joaoPinheiro/rafaelLedur/T2POO/T2PooApplicationTests.java
@@ -0,0 +1,13 @@
+package joaoPinheiro.rafaelLedur.T2POO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2PooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
