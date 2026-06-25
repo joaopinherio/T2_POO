3e4201c4bd5b9a1de2807fbc6627f59c5dd1cbf8
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-06-25 22:25:23 +0000

    push views branch

--- T2_POO/src/main/java/joaoPinheiro/rafaelLedur/T2POO/gui/TelaClienteRegView.java
+++ T2_POO/src/main/java/joaoPinheiro/rafaelLedur/T2POO/gui/TelaClienteRegView.java
@@ -27,11 +27,118 @@ import joaoPinheiro.rafaelLedur.T2POO.dados.Clientela;
 @PageTitle("Cadastro Clientes")
 @Route("cadastroCliente")
 public class TelaClienteRegView extends VerticalLayout {
-    private final Clientela clientela;
+    private final Clientela cliente;
+    
+    private final TextField nome;
+    private final TextField;
+    private final DatePicker dataNascimento;
+    private final ComboBox<String> pais;
+    private final Checkbox aceitaTermos;
+    
+    private final Grid<Pessoa> grid;
+
+    public TelaClienteRegView() {
+        // Inicializando o cadastro de pessoas
+        cadPessoas = CadastroPessoas.getInstance();
+
+        // Inicializando os campos do formulário
+        nome = new TextField("Nome");
+        email = new TextField("E-mail");
+        dataNascimento = new DatePicker("Data de nascimento");
+        pais = new ComboBox<>("País");
+        pais.setItems("Brasil", "Portugal", "EUA", "Inglaterra");
+
+        aceitaTermos = new Checkbox("Aceito os termos de serviço");
+        // inicializando o Grid para exibir as pessoas
+        grid = new Grid<>(Pessoa.class);
+
+        // Definindo as características do layout básico
+        setSpacing(true);
+        setPadding(true);
+
+        // Define título do formulário
+        add(new H2("Tela 1 - Cadastro de pessoas - INSERÇÃO"));
+
+        // Configuração do formulário
+        FormLayout formLayout = new FormLayout(nome, email, dataNascimento, pais, aceitaTermos);
+
+        // Definição dos botões de ação
+        Button salvarButton = new Button("Inserir", VaadinIcon.CHECK.create());
+        salvarButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        salvarButton.addClickShortcut(Key.ENTER);
+        salvarButton.addClickListener(click -> this.inserirFormulario());
+
+        Button cancelarButton = new Button("Cancelar");
+        Dialog dialogoCancelamento = criaDialogoDeCancelamento();
+        cancelarButton.addClickListener(click -> dialogoCancelamento.open());
+
+        // Adiciona botoes de ação em um layout horizontal
+        HorizontalLayout botoesLayout = new HorizontalLayout(salvarButton, cancelarButton);
+
+        // Configuração da Grid
+        grid.setItems(cadPessoas.getLista());
+//        grid.setColumns("nome", "email", "pais", "formattedDataNascimento");
+        grid.setColumns("nome", "email", "pais");
+        grid.addColumn(Pessoa::getFormattedDataNascimento).setHeader("Data nascimento");
+
+        // Monta todos os elementos na janela
+        add(formLayout, botoesLayout, new H2("Usuários Cadastrados"), grid);
+        add(new Hr());
+
+        // Define o botão de retorno à página principal
+        Button backButton = new Button("Voltar");
+        backButton.addClickListener(e -> UI.getCurrent().navigate(""));
+        add(backButton);
+    }
+
+    private void inserirFormulario() {
+        if (aceitaTermos.getValue() == false) {
+            Notification.show("Você precisa aceitar os termos de serviço.", 3000, Notification.Position.TOP_CENTER)
+                    .addThemeVariants(NotificationVariant.LUMO_ERROR);
+        } else {
+            if (nome.getValue().equals("") || email.getValue().equals("") ||
+                pais.getValue() == null || dataNascimento.getValue() == null) {
+                Notification.show("Erro! Campo vazio.", 3000, Notification.Position.BOTTOM_STRETCH);
+            } else {
+                Pessoa p = new Pessoa(nome.getValue(),
+                        email.getValue(),
+                        pais.getValue(),
+                        dataNascimento.getValue());
+                cadPessoas.cadastrar(p);
+                String mensagem = "Usuário " + p.getNome() + " salvo com sucesso!";
+                Notification.show(mensagem, 3000, Notification.Position.BOTTOM_STRETCH);
+            }
+            grid.getDataProvider().refreshAll();
+            limparFormulario();
+        }
+    }
+
+    private void limparFormulario() {
+        nome.clear();
+        dataNascimento.clear();
+        pais.clear();
+        email.clear();
+        nome.focus(); // Coloca o foco no campo nome
+    }
+
+    private Dialog criaDialogoDeCancelamento() {
+        Dialog dialogo = new Dialog();
+        dialogo.setHeaderTitle("Confirmar cancelamento");
+        dialogo.add(new Paragraph("Você tem certeza que deseja cancelar e limpar o formulário?"));
+        Button confirmarCancelamento = new Button("Sim, cancelar", e -> {
+            limparFormulario();
+            dialogo.close();
+        });
+        confirmarCancelamento.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_ERROR);
+        Button fecharDialogo = new Button("Não", e -> dialogo.close());
+        dialogo.getFooter().add(fecharDialogo, confirmarCancelamento);
+        return dialogo;
+    }
+}    private final Clientela clientela;
     //Componentes formulario
     private final TextField nome;
     private final TextField email;
 
-    private final Textfield cpf_cnpj;
+    private final TextField cpf_cnpj;
 
 }
